724da46670b2e64251f4b751cacdbe0b2d6fcd98
Connor Hindley <conn.hindley@gmail.com>
2014-10-14 10:41:03 -0400

    make directive more flexible

--- ng-polyglot.js
+++ ng-polyglot.js
@@ -9,6 +9,35 @@
     }
 }(this, function(Polyglot) {
 
+    function getAttrs($attrs) {
+        var attrFns = [],
+            attrRegex = /poly(\w.*$)/,
+            defaultFn = {
+                fn: 'html'
+            };
+        angular.forEach($attrs.$attrs, function(attr) {
+            if (attr === 'polyText') {
+                attrFns.push({
+                    fn: 'text'
+                });
+            } else if (attr === 'polyHtml') {
+                attrFns.push(defaultFn);
+            } else {
+                var matches = attrRegex.exec(attr);
+                if (matches && matches.length && matches.length === 2) {
+                    attrFns.push({
+                        fn: 'attr',
+                        attr: matches[1]
+                    });
+                }
+            }
+        });
+        if (!attrFns.length) {
+            attrFns.push(defaultFn);
+        }
+        return attrFns;
+    }
+
     angular.module('ngPolyglot', [])
         .constant('POLYGLOT_LANG', 'en')
         .factory('polyglotFactory', function() {
@@ -16,14 +45,27 @@
         })
         .directive('poly', ['polyglotFactory', 'POLYGLOT_LANG',
             function($Polyglot, lang) {
+                function link($scope, $el, $attrs) {
+                    var key = $attrs.poly;
+                    if (!key) {
+                        return console.log('No key provided to polyglot...');
+                    }
+                    var attrFns = getAttrs($attrs);
+                    $Polyglot.k(key, lang).then(function(value) {
+                        var $$el = angular.element($el);
+                        angular.forEach(attrFns, function(attrFn) {
+                            if (attrFn.fn && attrFn.attr) {
+                                $$el[attrFn.fn](attrFn.attr, value);
+                            } else if (attrFn.fn) {
+                                $$el[attrFn.fn](value);
+                            }
+                        });
+                    });
+                }
                 return {
                     restrict: 'A',
                     priority: 100,
-                    link: function($scope, $el, $attr) {
-                        $Polyglot.k($attr.poly, lang).then(function(value) {
-                            angular.element($el).html(value);
-                        });
-                    }
+                    link: link
                 };
             }
         ]);
